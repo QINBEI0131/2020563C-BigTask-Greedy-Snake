6bcac2b3a0565d8a4373a3efa17e3862b5022aa0
fanyi <1009078764@qq.com>
2022-06-14 11:38:28 +0800

    twoModify

---
+++ 2020563C-BigTask-Greedy-Snake/Components/GroundUnitType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    enum GroundUnitType
+    {
+        Blank = 0,//普通的背景方格
+        Food=1,//食物背景方格
+        Obstacle=2,//障碍物
+    }
+}
