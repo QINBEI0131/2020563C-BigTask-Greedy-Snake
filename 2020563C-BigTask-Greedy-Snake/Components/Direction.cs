8acd8bdeead60546ea5e4db936f25fb47867a371
fanyi <1009078764@qq.com>
2022-06-14 11:28:04 +0800

    firstCommit

---
+++ 2020563C-BigTask-Greedy-Snake/Components/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    enum Direction
+    {
+        //上下左右
+       Left=0,
+       Up=1,
+       Right=2,
+       Down=3
+    }
+}
