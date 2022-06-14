b7f070c90841e43e1fd3c39d4b7e28897da72019
fanyi <1009078764@qq.com>
2022-06-14 11:41:22 +0800

    fourModify

---
+++ 2020563C-BigTask-Greedy-Snake/Components/RandomMaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    class RandomMaker
+    {
+        private Random random;
+        public RandomMaker() 
+        {
+            this.random = new Random();
+        }
+        private int GetARandomRowIndex()
+        {
+            return random.Next(0, ToolHelp.RowCount);
+        }
+        private int GetARandomColIndex()
+        {
+            return random.Next(0, ToolHelp.ColCount);
+        }
+        //得到一个随机坐标
+        public Position GetARandomPosition()
+        {
+            Position p = new Position();
+            p.rowIndex = this.GetARandomRowIndex();
+            p.colIndex = this.GetARandomColIndex();
+            return p;
+        }
+    }
+}
