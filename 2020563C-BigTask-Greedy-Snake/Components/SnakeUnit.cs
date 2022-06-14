b52f3c0b4cf29ee0f9dae2c596c961589ab2e130
He12581 <105347966+He12581@users.noreply.github.com>
2022-06-14 11:41:58 +0800

    Add files via upload

---
+++ 2020563C-BigTask-Greedy-Snake/Components/SnakeUnit.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    class SnakeUnit
+    {
+        #region 身体单元坐标
+        private Position pos;
+        public  Position Pos { get => pos; set => pos = value; }
+        #endregion
+       
+        #region 身体单元颜色
+        //身体单元
+        //颜色
+        private Color color = Color.Green; public Color Color { get => color; set => color = value; }
+        
+
+        #endregion
+
+        #region 各种方式的提供函数们
+        public SnakeUnit(int rowIndex,int colIndex)
+        {
+            this.pos.rowIndex = rowIndex;
+            this.pos.colIndex = colIndex;
+
+        }
+        public SnakeUnit(int rowIndex, int colIndex,Color color)
+       :this(rowIndex,colIndex)
+        {
+            this.color = color;
+        }
+
+        public SnakeUnit (Position pos)
+        {
+            this.Pos = pos;
+        }
+        public SnakeUnit(Position pos,Color color)
+        :this(pos)
+        {
+            this.color = color;
+        }
+        #endregion
+
+        #region 根据当前自身单元绘制自己  
+        /// <summary>
+        /// 据自己的做标记及其颜色自己绘制
+        /// </summary>
+        /// <param name="g"></param>
+        public void DrawMe(Graphics g)
+        {
+            //将当前身体单元所在的单元格填充为当前身体单元对象的颜色
+            SolidBrush brush = new SolidBrush(this.color);
+            g.FillRectangle(brush,new Rectangle(this.pos.colIndex*ToolHelp.SquLength+1, this.pos.rowIndex * ToolHelp.SquLength + 1,
+                ToolHelp.SquLength-1, ToolHelp.SquLength - 1));
+
+
+        }
+        #endregion
+
+        #region 方向默认向左
+        
+        private Direction dir = Direction.Left;
+        internal Direction Dir { get => dir; set => dir = value; }
+      
+        #endregion
+
+        #region 上下左右移动的函数
+        public void MoveToLeft()
+        {
+            //left colpos-1
+            this.pos.colIndex--;
+
+        }
+        public void MoveToUp()
+        {
+            //up rowpos-1
+            this.pos.rowIndex--;
+
+        }
+        public void MoveToRight()
+        {
+            this.pos.colIndex++;
+
+        }
+        public void MoveToDown()
+        {
+            this.pos.rowIndex++;
+
+        }
+        #endregion
+
+        #region 根据当前身体所在单元所在的单元格的颜色填充为背景色
+        public void WipeMe(Graphics g)
+        {
+            //根据当前身体所在单元所在的单元格的颜色填充为背景色
+            SolidBrush brush = new SolidBrush(ToolHelp.Groundcolor);
+            g.FillRectangle(brush, new Rectangle(this.pos.colIndex * ToolHelp.SquLength + 1, this.pos.rowIndex * ToolHelp.SquLength + 1,
+                ToolHelp.SquLength - 1, ToolHelp.SquLength - 1));
+        }
+        #endregion
+
+        #region 身体单元移动的旧方向
+        private Direction oldDir = Direction.Left;
+        internal Direction OldDir { get => oldDir; set => oldDir = value; }
+        #endregion
+    }
+}
---
+++ 2020563C-BigTask-Greedy-Snake/Components/ToolHelp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    //提供所有需要共享的数据
+    static class ToolHelp
+    {
+    #region 游戏面板总行数14行 + static int RowCount
+        private static int rowCount = 14;
+            public static int RowCount { get => rowCount; set => rowCount = value; }
+        #endregion
+
+    #region 游戏面板总列数14行 + static int ColCount
+        private static int colCount = 14;
+
+        public static int ColCount { get => colCount; set => colCount = value; }
+        #endregion
+
+    #region 方块边长
+        private static int squLength;
+       public static int SquLength { get => squLength; set => squLength = value; }
+        #endregion
+
+    #region 初始化游戏环+ static  void InitializeEnvironment(int panelWidth, int panelHeight)
+        public static  void InitializeEnvironment(int panelWidth, int panelHeight)
+        {
+            //据面板宽度高度 总行数列数来为squlength赋值
+            //据传入宽度算出小方格宽度
+            int width = panelWidth / colCount;
+            //据传入宽度算出小方格高度
+            int height = panelHeight / rowCount;
+            //取较小变赋值边长
+            squLength = width > height ? height : width;
+        }
+        #endregion
+
+    #region 默认黑色线条
+        private static Color lineColor = Color.Black;
+        public static Color LineColor { get => lineColor; set => lineColor = value; }
+
+        #endregion
+
+    #region 默认蓝背景+static Color groundcolor
+        private static Color groundcolor = Color.Azure;
+        public static Color Groundcolor { get => groundcolor; set => groundcolor = value; }
+        #endregion
+
+    #region 食物默认颜色
+        private static Color foodColor = Color.Red;
+        public static Color FoodColor { get => foodColor; set => foodColor = value; }
+
+        #endregion
+        #region 分数变量
+        private static int score;
+        public static int Score { get =>ToolHelp. score; set =>ToolHelp.score = value; }
+        #endregion
+    }
+
+
+}
