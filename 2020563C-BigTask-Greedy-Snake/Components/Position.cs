f8f7af5182a630cfeaa4518351d67696a0a3f6a1
fanyi <1009078764@qq.com>
2022-06-14 11:40:08 +0800

    threeModify

---
+++ 2020563C-BigTask-Greedy-Snake/Components/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTaskTest01.Components
+{
+    struct Position
+    {
+        //身体单元坐标
+        //行列坐标
+        public int rowIndex;
+        public int colIndex;
+        //构造函数 初始化当前结构体对象
+        public Position(int rowIndex,int colIndex)
+        {
+            this.rowIndex = rowIndex;
+            this.colIndex = colIndex;
+        }
+    }
+}
